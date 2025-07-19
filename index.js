6d884736c0422a9f39bf7d98c158bbb74f69ea3d
nayeem2912 <farukomar2912@gmail.com>
2025-07-19 22:14:18 +0600

    edit

--- index.js
+++ index.js
@@ -763,9 +763,9 @@ app.delete('/blogs/:id', async (req, res) => {
     });
 
 
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
